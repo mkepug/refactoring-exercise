72836537757844aac8e3c4a0b34b78aa646b0043
Joel Clermont <jclermont@gmail.com>
2017-07-18 21:23:06 -0500

    fix missing line breaks

--- original.php
+++ original.php
@@ -1,11 +1,13 @@
 <?php
 public function onContentPrepare($context, &$article, &$params, $page = 0) {
   if ($this->_shouldNotTry($context, $article)){ return; }
-  $article->text = str_replace('{osapply}', $this->_oldButtonText($article->title), $article->text); $article->text = str_replace('{pkapply}', $this->_newButtonText($article->title), $article->text);
+  $article->text = str_replace('{osapply}', $this->_oldButtonText($article->title), $article->text); 
+  $article->text = str_replace('{pkapply}', $this->_newButtonText($article->title), $article->text);
 }
 
 private function _oldButtonText($title) {
-  $oldTitle = $this->_formatOldTitle($title); $baseText = $this->params->get('osapply'); return str_replace('{title}', $oldTitle, $baseText);
+  $oldTitle = $this->_formatOldTitle($title); $baseText = $this->params->get('osapply'); 
+  return str_replace('{title}', $oldTitle, $baseText);
 }
 
 private function _formatOldTitle($title) {
@@ -13,7 +15,9 @@ private function _formatOldTitle($title) {
 }
 
 private function _newButtonText($title) {
-  $newTitle = $this->_formatNewTitle($title); $baseText = $this->params->get('pkapply'); return str_replace('{title}', $newTitle, $baseText);
+  $newTitle = $this->_formatNewTitle($title); 
+  $baseText = $this->params->get('pkapply'); 
+  return str_replace('{title}', $newTitle, $baseText);
 }
 
 private function _formatNewTitle($title) {
