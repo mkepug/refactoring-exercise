4583b0290a35f10b4ce3b92de19b36274d9d5d5b
Aaron Saray <me@aaronsaray.com>
2017-07-23 13:59:32 -0500

    Full unit test coverage now.

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -17,13 +17,13 @@ use PHPUnit\Framework\TestCase;
 class PluginTest extends TestCase
 {
     /**
-     * @dataProvider onContentPrepareOsApplyProvider
+     * @dataProvider onContentPrepareProvider
      * @param $osApplyValue
      * @param $pkApplyValue
      * @param $article
      * @param $expectedText
      */
-    public function testOnContentPrepare_osapply($osApplyValue, $pkApplyValue, $article, $expectedText)
+    public function testOnContentPrepare($osApplyValue, $pkApplyValue, $article, $expectedText)
     {
         $plugin = new Plugin(new ParamsMock($osApplyValue, $pkApplyValue));
         $context = $params = null;
@@ -34,14 +34,26 @@ class PluginTest extends TestCase
     /**
      * @return array
      */
-    public function onContentPrepareOsApplyProvider()
+    public function onContentPrepareProvider()
     {
         return [
             ['ignore', 'ignore', (object) ['text' => 'content here with no replacements', 'title' => 'the title'], 'content here with no replacements'],
+            ['some value {title} end value', 'ignore', (object) ['text' => 'content here {title} more content', 'title' => 'the title here'], 'content here {title} more content'],
+            
             ['some value', 'ignore', (object) ['text' => 'content here {osapply} more content', 'title' => 'the title'], 'content here some value more content'],
             ['some value {title} end value', 'ignore', (object) ['text' => 'content here {osapply} more content', 'title' => 'title'], 'content here some value title end value more content'],
             ['some value {title} end value', 'ignore', (object) ['text' => 'content here {osapply} more content', 'title' => 'the title here'], 'content here some value the_title_here end value more content'],
-            ['some value {title} end value', 'ignore', (object) ['text' => 'content here {title} more content', 'title' => 'the title here'], 'content here {title} more content'],
+
+            ['ignore', 'some value', (object) ['text' => 'content here {pkapply} more content', 'title' => 'the title'], 'content here some value more content'],
+            ['ignore', 'some value {title} end value', (object) ['text' => 'content here {pkapply} more content', 'title' => 'title'], 'content here some value dGl0bGU= end value more content'],
+
+            [
+                'os_apply_value {title} and after', 
+                'pk_apply_value {title} end value', 
+                (object) ['text' => 'content here {osapply} more content {pkapply} end content', 
+                'title' => 'title here'], 
+                'content here os_apply_value title_here and after more content pk_apply_value dGl0bGUgaGVyZQ== end value end content'
+            ],
         ];
     }
 }
